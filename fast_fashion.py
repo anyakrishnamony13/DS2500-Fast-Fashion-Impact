fba28160165e53b087c2d396c74aedf83c6a53d9
anyakrishnamony13 <krishnamony.a@northeastern.edu>
2025-04-06 19:40:37 -0400

    initializing dataset

---
+++ fast_fashion.py
@@ -0,0 +1,15 @@
+'''
+DS 2500
+Final Project: Environmental Impact of Fast Fashion Brands
+4/1/25
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+FILENAME = 'Sustainable Fashion Export 2025-04-06 19-58-02.csv'
+fashion_df = pd.read_csv(FILENAME)
+
+def clean_data(df):
+    df = df
