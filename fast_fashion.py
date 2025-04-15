274dae1ff4b241d16206c53911ad16ee5e94430f
Patel-siya <patel.siya2@northeastern.edu>
2025-04-15 00:05:44 -0400

    Update fast_fashion.py

--- fast_fashion.py
+++ fast_fashion.py
@@ -9,11 +9,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-import matplotlib
-matplotlib.use('Agg')  
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 import warnings
